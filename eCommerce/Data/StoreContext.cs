1e363a0e474fe83ad48f0b84807142919d7a1366
Gledson <duarte.gledson@gmail.com>
2020-09-24 14:57:54 -0300

    Added basic API functinality

---
+++ eCommerce/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using eCommerce.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- eCommerce/Startup.cs
+++ eCommerce/Startup.cs
@@ -1,7 +1,9 @@
 using System;
+using eCommerce.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +23,10 @@ namespace eCommerce
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DevDatabase"));
+            });
             services.AddAntiforgery(
                 options =>
                 {
