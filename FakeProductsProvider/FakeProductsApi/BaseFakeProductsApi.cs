a9b2216c46efd459d4d5af73a79b9b92a6b62596
Gledson <duarte.gledson@gmail.com>
2020-10-17 10:49:45 -0300

    Added comments to the abstract base class of fake product default implementation

--- FakeProductsProvider/FakeProductsApi/BaseFakeProductsApi.cs
+++ FakeProductsProvider/FakeProductsApi/BaseFakeProductsApi.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using ProductsTransformer.FakeProductsApi;
+using FakeProductsProvider.DefaultFakeProducts;
 
 namespace FakeProductsProvider.FakeProductsApi
 {
+    
+    /// <summary>
+    /// Abstract class that provides default implement of retrieving fake products information
+    /// from a dedicate external web api 
+    /// </summary>
     public abstract class BaseFakeProductsApi
     {
         public string BaseUri { get; } = "https://fakestoreapi.com";
@@ -20,6 +25,12 @@ namespace FakeProductsProvider.FakeProductsApi
             return builder.ToString();
         }
 
+        /// <summary>
+        /// Retrieves an url composite from a base address and specific path which
+        /// represents the full url for getting a fake product list
+        /// </summary>
+        /// <param name="productsType">The product types enum</param>
+        /// <returns>An url string</returns>
         public string UriByProductType(ProductsTypes productsType)
         {
             return productsType switch
@@ -29,6 +40,13 @@ namespace FakeProductsProvider.FakeProductsApi
             };
         }
 
+        /// <summary>
+        /// Retrieves an url composite from a base address and specific path which
+        /// represents the full url for getting a single fake product based on its id
+        /// </summary>
+        /// <param name="productsType">The product types enum</param>
+        /// <param name="id">The id correspondent to the fake api rules</param>
+        /// <returns>An url string</returns>
         public string UriByProductType(ProductsTypes productsType, int id)
         {
             return productsType switch
