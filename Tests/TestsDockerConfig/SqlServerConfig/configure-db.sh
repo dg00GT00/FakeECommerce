0f1f656ef488042de401e9316b4f666b9d55d85e
Gledson <duarte.gledson@gmail.com>
2020-11-02 16:29:18 -0300

    Removed support to auto generate script in SQL Server Database

--- Tests/TestsDockerConfig/SqlServerConfig/configure-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# wait for MSSQL server to start
-threshold=30
-echo "Waiting $threshold seconds until server fully initialized..."
-sleep $threshold && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -i $SQLSERVER_CONFIG_PWD/setup.sql
-echo "Created default setup to unit testing purposes"
--- Tests/TestsDockerConfig/SqlServerConfig/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start the script to create the DB and user
-$SQLSERVER_CONFIG_PWD/configure-db.sh &
-
-# Start SQL Server
-# The command that starts the server should be at the very end of this entrypoint
-/opt/mssql/bin/sqlservr
