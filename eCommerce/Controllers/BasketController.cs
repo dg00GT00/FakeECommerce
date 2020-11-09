c9f4952f995042ac69ef4f2b4a402b3135b1b456
Gledson <duarte.gledson@gmail.com>
2020-11-09 17:44:37 -0300

    Created basket controller

---
+++ eCommerce/Controllers/BasketController.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+        {
+            var basket = await _basketRepository.GetBasketAsync(id);
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            return CreatedAtAction(nameof(GetBasketById), new {updatedBasket.Id}, updatedBasket);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteBasketAsync(string id)
+        {
+            var deleted = await _basketRepository.DeleteBasketAsync(id);
+            return deleted ? (ActionResult) NoContent() : NotFound();
+        }
+    }
+}
