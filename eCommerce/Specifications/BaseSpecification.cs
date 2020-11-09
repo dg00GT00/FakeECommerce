81ea437c26977c716af5c1ed1e8b33bb24b344ee
Gledson <duarte.gledson@gmail.com>
2020-11-09 09:15:32 -0300

    Changed return type of Include method on base specification class to IList

--- eCommerce/Specifications/BaseSpecification.cs
+++ eCommerce/Specifications/BaseSpecification.cs
@@ -8,13 +8,13 @@ namespace eCommerce.Specifications
     public class BaseSpecification<T> : ISpecification<T>
     {
         public int Take { get; private set; }
-        
+
         public int Skip { get; private set; }
-        
+
         public bool IsPagingEnabled { get; private set; }
         public Expression<Func<T, bool>> Criteria { get; protected set; }
 
-        public IEnumerable<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
 
         public Expression<Func<T, object>> OrderBy { get; private set; }
 
@@ -27,14 +27,14 @@ namespace eCommerce.Specifications
 
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
-            ((List<Expression<Func<T, object>>>) Includes).Add(includeExpression);
+            Includes.Add(includeExpression);
         }
 
         protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
         {
             OrderBy = orderByExpression;
         }
-        
+
         protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
         {
             OrderByDescending = orderByDescExpression;
