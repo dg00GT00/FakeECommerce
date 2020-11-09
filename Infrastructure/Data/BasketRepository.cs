c55bfbf7086c295e1c2c92b70ebc76db58d26d4c
Gledson <duarte.gledson@gmail.com>
2020-11-09 17:10:33 -0300

    Implemented customer basket repository

--- Infrastructure/Data/BasketRepository.cs
+++ Infrastructure/Data/BasketRepository.cs
@@ -1,24 +1,42 @@
+using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using StackExchange.Redis;
 
 namespace Infrastructure.Data
 {
     public class BasketRepository : IBasketRepository
     {
+        private readonly IDatabase _database;
+
+        public BasketRepository(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+
         public async Task<CustomerBasket> GetBasketAsync(string basketId)
         {
-            throw new System.NotImplementedException();
+            var data = await _database.StringGetAsync(basketId);
+            return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(data);
         }
 
         public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
         {
-            throw new System.NotImplementedException();
+            var created =
+                await _database.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(5));
+            if (!created)
+            {
+                return null;
+            }
+
+            return await GetBasketAsync(basket.Id);
         }
 
         public async Task<bool> DeleteBasketAsync(string basketId)
         {
-            throw new System.NotImplementedException();
+            return await _database.KeyDeleteAsync(basketId);
         }
     }
 }
