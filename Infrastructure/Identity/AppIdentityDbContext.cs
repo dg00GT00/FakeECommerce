73c2a0609cc665ec113d06b2ff44e74f4acfc468
Gledson <duarte.gledson@gmail.com>
2020-11-10 08:23:19 -0300

    Implemented Identity context to the application user

---
+++ Infrastructure/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,19 @@
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+        }
+
+        // It's needed specify an override to this method for Identity constraints purposes
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
