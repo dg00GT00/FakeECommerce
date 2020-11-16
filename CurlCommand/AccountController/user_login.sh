ccccd03dd265cca2211f20de8fd0aca1920dd9d1
Gledson <duarte.gledson@gmail.com>
2020-11-16 14:34:56 -0300

    Updated the curl commands to include user account management

--- CurlCommand/AccountController/user_login.sh
+++ CurlCommand/AccountController/user_login.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-input=$(dirname $0)/post_basket.json
+input=$(dirname $0)/user_login.json
 
 # Test Basket controller post method 
 
@@ -8,4 +8,4 @@ curl \
 -d "@$input" \
 -H 'Content-Type: application/json; charset=UTF-8' \
 --cacert $HOME/CA/sub-ca/certs/sub-ca.crt \
---http2 -v "https://localhost:5001/api/basket" | python3 -m json.tool
+--http2 -v "https://localhost:5001/api/account/login" | python3 -m json.tool
--- CurlCommand/BasketController/delete_basket.sh
+++ CurlCommand/BasketController/delete_basket.sh
@@ -3,5 +3,5 @@
 # Test Basket controller delete method 
 
 curl -X DELETE \
---cacert ~/LocalHostCertificate/myCA.pem \
---http2 -v "https://localhost:5001/api/basket?id=basket2" | python3 -m json.tool | echo
+--cacert $HOME/CA/sub-ca/certs/sub-ca.crt \
+--http2 -v "https://localhost:5001/api/basket?id=basket2" | python3 -m json.tool
