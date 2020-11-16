fbf6e9d58c681e80bfc0865ad459b6e50bb0aa76
Gledson <duarte.gledson@gmail.com>
2020-11-16 14:33:36 -0300

    Created controller to manager the user accounts

---
+++ eCommerce/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using System.Threading.Tasks;
+using Core.Dtos;
+using Core.Entities.Identity;
+using eCommerce.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IHttpContextAccessor _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+            IHttpContextAccessor context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null)
+            {
+                return Unauthorized(new ApiResponse(401));
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized(new ApiResponse(401));
+            }
+
+            _context.HttpContext.Response.StatusCode = (int) HttpStatusCode.Created;
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "This will be a token",
+                DisplayName = user.DisplayName
+            };
+        }
+    }
+}
