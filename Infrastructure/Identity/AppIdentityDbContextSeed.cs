fa1d70877216e932a56f47e9eb7e48faad8d7be1
Gledson <duarte.gledson@gmail.com>
2020-11-16 14:30:05 -0300

    Made a simple user seeding

--- Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -1,47 +1,59 @@
-using System;
 using System.Linq;
-using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities.Identity;
 using Microsoft.AspNetCore.Identity;
 
 namespace Infrastructure.Identity
 {
-    public class AppIdentityDbContextSeed : IDisposable
+    public class AppIdentityDbContextSeed
     {
-        private readonly HttpClient _httpClient;
+        // public AppIdentityDbContextSeed(string certificatePath, string password)
+        // {
+        //     _httpClient = ConfigureHttpClient(certificatePath, password);
+        // }
 
-        public AppIdentityDbContextSeed()
-        {
-            _httpClient = new HttpClient {BaseAddress = new Uri("https://localhost:5002/fake/")};
-        }
+        // private static HttpClient ConfigureHttpClient(string certificatePath, string password)
+        // {
+        //     var handler = new HttpClientHandler();
+        //     handler.ClientCertificates.Add(new X509Certificate2("/home/dggt/CA/client/certs/netwebappclient.pfx",
+        //         "viper1081"));
+        //     return new HttpClient(handler) {BaseAddress = new Uri("https://localhost:5002/fake/")};
+        // }
 
-        private async Task<AppUser> GenerateFakeUser(string displayName)
-        {
-            var userJson = await _httpClient.GetStringAsync("user");
-            var user = JsonSerializer.Deserialize<dynamic>(userJson);
-            return new AppUser
-            {
-                DisplayName = displayName,
-                Email = user.Email,
-                UserName = user.Email,
-                Address = user.Address
-            };
-        }
+        // private async Task<AppUser> GenerateFakeUser(string displayName)
+        // {
+        //     var userJson = await _httpClient.GetStringAsync("user");
+        //     var user = JsonSerializer.Deserialize<dynamic>(userJson);
+        //     return new AppUser
+        //     {
+        //         DisplayName = displayName,
+        //         Email = user.Email,
+        //         DisplayName = user.Email,
+        //         Address = user.Address
+        //     };
+        // }
 
         public async Task SeedUserAsync(UserManager<AppUser> userManager)
         {
             if (!userManager.Users.Any())
             {
-                var user = await GenerateFakeUser("Bob");
+                var user = new AppUser
+                {
+                    DisplayName = "Bob",
+                    Email = "bob@test.com",
+                    UserName = "bob@test.com",
+                    Address = new Address
+                    {
+                        FirstName = "Bob",
+                        LastName = "Bobbity",
+                        Street = "10 The Street",
+                        City = "New York",
+                        State = "NY",
+                        ZipCode = "90210"
+                    }
+                };
                 await userManager.CreateAsync(user, "Pa$$w0rd");
             }
-        }
-
-        public void Dispose()
-        {
-            _httpClient.Dispose();
         }
     }
 }
