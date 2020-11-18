f541e30f954a04de07c6f63aba01eaa28b4ea723
Gledson <duarte.gledson@gmail.com>
2020-11-18 08:51:23 -0300

    Added JWT Authorization handler to Swagger

--- eCommerce/Extensions/SwaggerServiceExtensions.cs
+++ eCommerce/Extensions/SwaggerServiceExtensions.cs
@@ -11,6 +11,25 @@ namespace eCommerce.Extensions
             s.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc("v1", new OpenApiInfo {Title = "FakeeCommerce", Version = "v1"});
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                options.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    {securitySchema, new[] {"Bearer"}}
+                };
+                options.AddSecurityRequirement(securityRequirement);
             });
             return s;
         }
