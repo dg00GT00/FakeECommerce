9631e3cf0647078484b6aa848c34413a621fa2f3
Gledson <duarte.gledson@gmail.com>
2020-11-18 17:27:45 -0300

    Implemented order service

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
 using Core.Entities.OrderAggregate;
 using Core.Interfaces;
 
@@ -7,10 +9,45 @@ namespace Infrastructure.Services
 {
     public class OrderService : IOrderService
     {
-        public async Task<Order> CreateOrder(string buyerEmail, int deliveryMethod, string basketId,
+        private readonly IGetRepository<Order> _orderRepo;
+        private readonly IGetRepository<DeliveryMethod> _dmRepo;
+        private readonly IGetRepository<Product> _productRepo;
+        private readonly IBasketRepository _basketRepo;
+
+        public OrderService(IGetRepository<Order> orderRepo, IGetRepository<DeliveryMethod> dmRepo,
+            IGetRepository<Product> productRepo, IBasketRepository basketRepo)
+        {
+            _dmRepo = dmRepo;
+            _orderRepo = orderRepo;
+            _productRepo = productRepo;
+            _basketRepo = basketRepo;
+        }
+
+        public async Task<Order> CreateOrder(string buyerEmail, int deliveryMethodId, string basketId,
             Address shippingAddress)
         {
-            throw new System.NotImplementedException();
+            // Get basket from the repo
+            var basket = await _basketRepo.GetBasketAsync(basketId);
+            // Get items from the product repo
+            var items = new List<OrderItem>();
+            foreach (var item in basket.Items)
+            {
+                var productItem = await _productRepo.GetEntityByIdAsync(item.Id);
+                var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name, productItem.PictureUrl);
+                var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+                items.Add(orderItem);
+            }
+
+            // Get delivery method from repo
+            var deliveryMethod = await _dmRepo.GetEntityByIdAsync(deliveryMethodId);
+            // Calculate subtotal 
+            var subtotal = items.Sum(item => item.Price * item.Quantity);
+            // Create order
+            var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+            // TODO: Save to db
+
+            // Return the order
+            return order;
         }
 
         public async Task<IReadOnlyList<Order>> GetOrderForUserAsync(string buyerEmail)
