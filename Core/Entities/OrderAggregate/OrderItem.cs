62597e073378fe1cf73988ef0909a3fad37202bd
Gledson <duarte.gledson@gmail.com>
2020-11-18 09:07:25 -0300

    Created the order aggregate classes

---
+++ Core/Entities/OrderAggregate/OrderItem.cs
@@ -0,0 +1,21 @@
+namespace Core.Entities.OrderAggregate
+{
+    public class OrderItem : BaseEntity
+    {
+        // To Entity Framework requirements
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(ProductItemOrdered itemOrdered, decimal price, int quantity)
+        {
+            ItemOrdered = itemOrdered;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public ProductItemOrdered ItemOrdered { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
