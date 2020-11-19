805f5f671a661cf4c56cbff365299e71b0c563f8
Gledson <duarte.gledson@gmail.com>
2020-11-19 09:57:31 -0300

    Added more curl commands and updated the existence ones to accomodate more endpoint tests

--- CurlCommand/OrderController/post_order.sh
+++ CurlCommand/OrderController/post_order.sh
@@ -5,6 +5,6 @@ input=$(dirname $0)/post_order.json
 curl \
 -d "@$input" \
 -H 'Content-Type: application/json; charset=UTF-8' \
--H 'Authorization: Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJvYkB0ZXN0LmNvbSIsImdpdmVuX25hbWUiOiJCb2IiLCJuYmYiOjE2MDU3MzkyODMsImV4cCI6MTYwNTc0NjQ4MywiaWF0IjoxNjA1NzM5MjgzLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo1MDAxIn0.RUwQE5Up8F5lpWZJVfR5PdESN74OTFZCVYHE2j4FdmlMqbLLgRWeen6pXKEyHIxEPLb7ZwyXgGSJG7d6y6yAwQ' \
+-H 'Authorization: Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJvYkB0ZXN0LmNvbSIsImdpdmVuX25hbWUiOiJCb2IiLCJuYmYiOjE2MDU3ODg4MzIsImV4cCI6MTYwNTc5NjAzMiwiaWF0IjoxNjA1Nzg4ODMyLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo1MDAxIn0.OAyiPaDApLP_T13mscl1O2KI-gQ4VF4YlPiRMsn7njfhskMpPF9-Z3NKvagZQ0M0ldbEyfEGYdZ-s_Q74g9IpQ' \
 --cacert $HOME/CA/sub-ca/certs/sub-ca.crt \
 --http2 -v "https://localhost:5001/api/orders" | python3 -m json.tool
---
+++ CurlCommand/ProductController/post_product.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+input=$(dirname $0)/post_product.json
+
+curl \
+-d "@$input" \
+-H 'Content-Type: application/json; charset=UTF-8' \
+--cacert $HOME/CA/sub-ca/certs/sub-ca.crt \
+--http2 -v "https://localhost:5001/api/products" | python3 -m json.tool
