8b76a3b321d13867af7bf8631241eb2681cbf090
Gledson Duarte <duarte.gledson@gmail.com>
2021-03-11 16:49:10 -0300

    Removed unnecessary code from the Identity context seeding

--- Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -7,32 +7,6 @@ namespace Infrastructure.Identity
 {
     public class AppIdentityDbContextSeed
     {
-        // public AppIdentityDbContextSeed(string certificatePath, string password)
-        // {
-        //     _httpClient = ConfigureHttpClient(certificatePath, password);
-        // }
-
-        // private static HttpClient ConfigureHttpClient(string certificatePath, string password)
-        // {
-        //     var handler = new HttpClientHandler();
-        //     handler.ClientCertificates.Add(new X509Certificate2("/home/dggt/CA/client/certs/netwebappclient.pfx",
-        //         "viper1081"));
-        //     return new HttpClient(handler) {BaseAddress = new Uri("https://localhost:5002/fake/")};
-        // }
-
-        // private async Task<AppUser> GenerateFakeUser(string displayName)
-        // {
-        //     var userJson = await _httpClient.GetStringAsync("user");
-        //     var user = JsonSerializer.Deserialize<dynamic>(userJson);
-        //     return new AppUser
-        //     {
-        //         DisplayName = displayName,
-        //         Email = user.Email,
-        //         DisplayName = user.Email,
-        //         Address = user.Address
-        //     };
-        // }
-
         public async Task SeedUserAsync(UserManager<AppUser> userManager)
         {
             if (!userManager.Users.Any())
